8b53c7b4fdc933e6f3929789cfdfb5f59f0eb190
Gabriel-Krieger <82594590+Gabriel-Krieger@users.noreply.github.com>
2021-06-06 18:49:57 +0200

    Update Calculator.java
    Added a feature:
    If you try to divide by 0 it returns a 0

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -2,6 +2,7 @@ public class Calculator {
 	static int solution;
 
 	public static int calculator(int num1, char operator, int num2) {
+		try {
 		if(operator == '+') {
 			solution = num1 + num2;
 			} else if(operator == '-') {
@@ -10,11 +11,19 @@ public class Calculator {
 			solution = num1 * num2;
 		} else if(operator == '/') {
 			solution = num1 / num2;
+		} else if(operator == '/' && num1 == 0) {
+			solution = 0;
+		} else if(operator == '/' && num2 == 0) {
+			solution = 0;
 		}
 		return solution;
+		} catch(ArithmeticException e) {
+		return 0;
+		}
+		
   }
 	
 	public static void main(String args[]) {
-		System.out.println(calculator(5, '*', 2));
+		System.out.println(calculator(4, '/', 0));
 	}
 }
